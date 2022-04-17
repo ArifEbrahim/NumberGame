00241daf96ccd3bcecbbe52673628c97aa1eee22
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-17 13:09:56 +0100

    feature: created and styled text input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View>
-      <Text>StartGameScreen</Text>
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16, 
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    marginHorizontal: 24,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.5
+  }
+})
