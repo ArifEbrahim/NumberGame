e2dbf08a4687489f849117a058b5e1427f0653db
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-17 13:53:37 +0100

    feature: further styling of text input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,11 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,14 +17,25 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    padding: 16, 
+    padding: 16,
     marginTop: 100,
-    backgroundColor: '#72063c',
+    backgroundColor: "#72063c",
     borderRadius: 8,
     marginHorizontal: 24,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
-    shadowOpacity: 0.5
-  }
-})
+    shadowOpacity: 0.5,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
