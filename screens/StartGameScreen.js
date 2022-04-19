894c31ebb9ff671500f553b9c0b5668941f6d418
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-19 19:36:37 +0100

    feature: added state to main number and created confirm handler

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,29 @@
 import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 export default function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const numberInputHandler = (enteredText) => setEnteredNumber(enteredText);
+
+  const confirmInputHandler = () => {}
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.numberInput}
         maxLength={2}
         keyboardType="number-pad"
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
           <PrimaryButton>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
@@ -23,8 +32,8 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginTop: 100,
     backgroundColor: "#3b021f",
@@ -47,9 +56,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   buttonsContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   buttonContainer: {
     flex: 1,
-  }
+  },
 });
