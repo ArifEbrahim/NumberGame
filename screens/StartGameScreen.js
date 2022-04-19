9e074366711ed56e6dab90790979375668aecce7
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-19 22:30:04 +0100

    feature: added input validation for number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
@@ -7,7 +7,20 @@ export default function StartGameScreen() {
 
   const numberInputHandler = (enteredText) => setEnteredNumber(enteredText);
 
-  const confirmInputHandler = () => {}
+  const resetInputHandler = () => setEnteredNumber('');
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber)
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number',
+        'Number has to be a number between 1 and 99',
+        [{text: 'OK', style: 'destructive', onPress: resetInputHandler}]
+      )
+      return;
+    }
+  };
 
   return (
     <View style={styles.inputContainer}>
@@ -20,7 +33,7 @@ export default function StartGameScreen() {
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
